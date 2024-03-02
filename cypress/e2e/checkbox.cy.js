6b0d4a39eec4cf358457c0143a5dbe772b245b53
Kasiaglocka <kasia.glocka@gmail.com>
2024-03-02 20:20:12 +0100

    asercje- weryfikacja czy too co dzieje się na stronie i to co chcemy żeby sie działo faktycznie ma miejsce

--- cypress/e2e/checkbox.cy.js
+++ cypress/e2e/checkbox.cy.js
@@ -4,10 +4,12 @@ describe("Test związany z zaznaczaniem checkboxa",() => {
     it("Kliknięcie w zakładkę Women", () => {
         cy.visit("/");
         cy.get(`a[title="Women"]`).click();
+        cy.url().should("include", "id_category=3&controller=category")
     })
     
     it("Zaznaczenie checkboxa w zakładce Women", () => {
         cy.get("#layered_category_4").check();
+        cy.get("#layered_category_4").should("be.checked")
         cy.get("#layered_category_8").check();
         cy.get('#ul_layered_id_attribute_group_1 input').check();
     })
--- cypress/e2e/wpisywanie.cy.js
+++ cypress/e2e/wpisywanie.cy.js
@@ -4,9 +4,13 @@ describe("Wpisywanie tekstu w wyszukiwarke", () => {
     it("Wpisywanie tekstu", () => {
         cy.visit("/");
         cy.get("#search_query_top").type("Przykładowa treść")
+        cy.get("#search_query_top").should("have.value", "Przykładowa treść")
         cy.wait(3000)
         cy.get("#search_query_top").clear();
         cy.wait(3000)
         cy.get("#search_query_top").type("Przykłądowa treść{enter}")
+        cy.get("p.alert").should("be.visible").and("include.text", "No results")
+        cy.get("#search_query_top").should("have.class", "search_query")
+        cy.get("#search_query_top").should("have.css", "margin_right", "1px")
     })
 })
