84bb3530673c7b469a2fa13018ba6bf2ba0beef7
Kasiaglocka <kasia.glocka@gmail.com>
2024-03-14 14:55:36 +0100

    Page Object Model- ćwiczenia

--- cypress/e2e/wpisywanie.cy.js
+++ cypress/e2e/wpisywanie.cy.js
@@ -1,7 +1,8 @@
 /// <reference types="cypress" />
 import Base from "../pages/Base";
-import Search from "../pages/Search"
-import { searchPhrase } from "../fixtures/searchData.json"
+import Search from "../pages/Search";
+import ResultPage from "../pages/ResultPage";
+import { searchPhrase, notFoundProduct } from "../fixtures/searchData.json"
 
 describe("Wpisywanie tekstu w wyszukiwarke", () => {
     it("Wpisywanie tekstu", () => {
@@ -11,8 +12,8 @@ describe("Wpisywanie tekstu w wyszukiwarke", () => {
         cy.wait(3000)
         Search.clearSearchPhrase();
         cy.wait(3000)
-        cy.get("#search_query_top").type("Przykłądowa treść{enter}")
-        cy.get("p.alert").should("be.visible").and("include.text", "No results")
+        Search.typeSearchPhrase(`${searchPhrase} {enter}`)
+        ResultPage.should("be.visible").and("include.text", notFoundProduct)
         cy.get("#search_query_top").should("have.class", "search_query")
         cy.get("#search_query_top").should("have.css", "margin_right", "1px")
     })
